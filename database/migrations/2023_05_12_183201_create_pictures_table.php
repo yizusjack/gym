c10258e46232926fc35ea1170301b94e3bcb7c71
Roberto Flores <rob.floresm04@gmail.com>
2023-09-18 21:47:58 -0600

    Columnas bien

--- database/migrations/2023_05_12_183201_create_pictures_table.php
+++ database/migrations/2023_05_12_183201_create_pictures_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('extension');
             $table->string('mime');
             $table->foreignId('gimnastas_id')->constrained()->onDelete('cascade');
-            $table->boolean('approved')->default(false);
         });
     }
 
